96c8cc4522bb32917150295d60a4bc2655f98e97
6ak5 <push4limit@gmail.com>
2023-06-16 12:43:22 +0000

    Added privat domain suffix like pp.ua and fixed auth for wildcard of subdomains

--- posthook_auth.py
+++ posthook_auth.py
@@ -1,27 +1,59 @@
 #!/usr/bin/python3
+
 import requests
 import json
 import sys
 from os import environ
 import time
-import configparser 
+import configparser
+import tldextract
 
-config = configparser.ConfigParser() 
+config = configparser.ConfigParser()
 config.read("settings.ini")
-api_key=(config["api.websa.com"]["api_key"])
+api_key = config["api.websa.com"]["api_key"]
 
 certbot_domain = environ["CERTBOT_DOMAIN"]
 certbot_validation = environ["CERTBOT_VALIDATION"]
 
-response = requests.get("https://api.websa.com/api/v1/dns_zones?page=1&q%5Bsorts%5D=updated_at+desc", headers={"Authorization":api_key})
+extract = tldextract.TLDExtract(include_psl_private_domains=True)
+extracted_domain = extract(certbot_domain)
+
+subdomain = extracted_domain.subdomain
+target_domain = extracted_domain.domain + "." + extracted_domain.suffix
+
+response = requests.get("https://api.websa.com/api/v1/dns_zones?page=1&q%5Bsorts%5D=updated_at+desc", headers={"Authorization": api_key})
 JsonData = json.loads(response.text)
 
 for i in JsonData["dns_zones"]:
-    if i["name"] == certbot_domain:
-    	dns_zone_id=i["id"]
+    if i["name"] == target_domain:
+        dns_zone_id = i["id"]
 
-requests.put(("https://api.websa.com/api/v1/dns_zones/%s" %(dns_zone_id)), 
-	data=('{"dns_zone":{"records":[{"id":"","name":"_acme-challenge","ttl":60,"type":"TXT","priority":null,"weight":null,"port":null,"value":"%s"}]}}' %(certbot_validation))   , 
-	headers={"Content-Type":"application/json","Authorization":api_key })
+acme_challenge_name = "_acme-challenge"
+if subdomain:
+    acme_challenge_name += "." + subdomain
+
+data = {
+    "dns_zone": {
+        "records": [
+            {
+                "id": "",
+                "name": acme_challenge_name,
+                "ttl": 60,
+                "type": "TXT",
+                "priority": None,
+                "weight": None,
+                "port": None,
+                "value": certbot_validation
+            }
+        ]
+    }
+}
+
+requests.put(
+    "https://api.websa.com/api/v1/dns_zones/%s" % dns_zone_id,
+    data=json.dumps(data),
+    headers={"Content-Type": "application/json", "Authorization": api_key}
+)
 
 time.sleep(30)
+
--- posthook_clean.py
+++ posthook_clean.py
@@ -1,31 +1,48 @@
 #!/usr/bin/python3
+
 import requests
 import json
 import sys
 from os import environ
 import time
-import configparser 
+import configparser
+import tldextract
 
-config = configparser.ConfigParser() 
+config = configparser.ConfigParser()
 config.read("settings.ini")
-api_key=(config["api.websa.com"]["api_key"])
+api_key = config["api.websa.com"]["api_key"]
 
 certbot_domain = environ["CERTBOT_DOMAIN"]
 
-response = requests.get("https://api.websa.com/api/v1/dns_zones?page=1&q%5Bsorts%5D=updated_at+desc", headers={"Authorization":api_key})
+extract = tldextract.TLDExtract(include_psl_private_domains=True)
+extracted_domain = extract(certbot_domain)
+
+subdomain = extracted_domain.subdomain
+target_domain = extracted_domain.domain + "." + extracted_domain.suffix
+
+response = requests.get("https://api.websa.com/api/v1/dns_zones?page=1&q%5Bsorts%5D=updated_at+desc", headers={"Authorization": api_key})
 JsonData = json.loads(response.text)
 
 for i in JsonData["dns_zones"]:
-    if i["name"] == certbot_domain:
-    	dns_zone_id=i["id"]
+    if i["name"] == target_domain:
+        dns_zone_id = i["id"]
 
+acme_challenge_name = "_acme-challenge"
+if subdomain:
+    acme_challenge_name += "." + subdomain
+
+acme_id = None
 for i in JsonData["dns_zones"]:
-	for b in i["records"]:
-		if b["name"] =="_acme-challenge":
-				acme_id=(b["id"])
+    for b in i["records"]:
+        if b["name"].startswith(acme_challenge_name):
+            acme_id = b["id"]
 
-requests.put(("https://api.websa.com/api/v1/dns_zones/%s" %(dns_zone_id)), 
-	data=('{"dns_zone":{"records": [{"id":"%s", "_destroy": "true"}]}}' %(acme_id)),
-	headers={"Content-Type":"application/json","Authorization":api_key })
+if acme_id:
+    requests.put(
+        "https://api.websa.com/api/v1/dns_zones/%s" % dns_zone_id,
+        data='{"dns_zone":{"records": [{"id":"%s", "_destroy": "true"}]}}' % acme_id,
+        headers={"Content-Type": "application/json", "Authorization": api_key}
+    )
 
 time.sleep(30)
+
